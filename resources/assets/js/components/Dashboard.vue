c3403350e20ca60b48932547d9ed09caf3409807
Neciu Dan <cst2989@gmail.com>
2017-10-22 11:00:48 +0300

    rearenged in dashboard

--- resources/assets/js/components/Dashboard.vue
+++ resources/assets/js/components/Dashboard.vue
@@ -1,37 +1,46 @@
 <template>
 <div class="dashboard">
     <div class="row tile_count">
-        <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
-              <span class="count_top"><i class="fa fa-money"></i> Total revenue</span>
-              <div class="count">{{revenue}}</div>
-              <span class="count_bottom"><i class="green">4% </i> From last Week</span>
+        <div class="col-md-4">
+            <div class="col-md-12 col-sm-4 col-xs-6 tile_stats_count">
+            </div>
+            <div class="col-md-10 col-md-offset-2 col-sm-4 col-xs-6 tile_stats_count">
+                  <span class="count_top"><i class="fa fa-money"></i> Total revenue</span>
+                  <div class="count">{{revenue}}</div>
+                  <span class="count_bottom"><i class="green">4% </i> From last Week</span>
+            </div>
+            <div class="col-md-10 col-md-offset-2 col-sm-4 col-xs-6 tile_stats_count">
+                  <span class="count_top"><i class="fa fa-line-chart"></i> Total Hashrate</span>
+                  <div class="count">{{hashrate}}</div>
+                  <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>3% </i> From last Week</span>
+            </div>
         </div>
-        <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
-              <span class="count_top"><i class="fa fa-line-chart"></i> Total Hashrate</span>
-              <div class="count">{{hashrate}}</div>
-              <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>3% </i> From last Week</span>
-        </div>
-        <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
-              <span class="count_top"><i class="fa fa-user"></i> Nr of Authors</span>
-              <div class="count">{{nr}}</div>
-              <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>23% </i> From last Week</span>
-        </div>
-        <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
-              <span class="count_top"><i class="fa fa-user"></i> Paid Authors</span>
-              <div class="count">{{authorsPaid}}</div>
-              <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>13% </i> From last Week</span>
-        </div>
-        <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
-              <span class="count_top"><i class="fa fa-user"></i> Paid Readers</span>
-              <div class="count">{{readers}}</div>
-              <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>13% </i> From last Week</span>
-        </div>
-    </div>
-    <div class="row">
         <div class="col-md-4">
             <pie name="Sites Revenue" :labels="labels1" :colors="colors" :data="data1"></pie>
         </div>
         <div class="col-md-4">
+                    <div class="col-md-2  col-sm-4 col-xs-6 tile_stats_count">
+                    </div>
+                    <div class="col-md-10 col-sm-4 col-xs-6 tile_stats_count">
+                          <span class="count_top"><i class="fa fa-user"></i> Nr of Authors</span>
+                          <div class="count">{{nr}}</div>
+                          <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>23% </i> From last Week</span>
+                    </div>
+                    <div class="col-md-10 col-md-offset-2 col-sm-4 col-xs-6 tile_stats_count">
+                          <span class="count_top"><i class="fa fa-user"></i> Paid Authors</span>
+                          <div class="count">{{authorsPaid}}</div>
+                          <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>13% </i> From last Week</span>
+                    </div>
+                    <div class="col-md-10 col-md-offset-2 col-sm-4 col-xs-6 tile_stats_count">
+                          <span class="count_top"><i class="fa fa-user"></i> Paid Readers</span>
+                          <div class="count">{{readers}}</div>
+                          <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>13% </i> From last Week</span>
+                    </div>
+        </div>
+
+    </div>
+    <div class="row">
+        <div class="col-md-4 col-md-offset-2">
             <chart name="Authors Revenue" :labels="labels2" color="#7886f9" :data="data2"></chart>
         </div>
         <div class="col-md-4">
