5dcf9f4bd80de9fc4d17979a1211a62402f3dbf9
Adrian Marin <adrian@adrianthedev.com>
2017-10-22 13:10:21 +0300

    redone seeds

--- database/seeds/DummySeeder.php
+++ database/seeds/DummySeeder.php
@@ -11,7 +11,31 @@ class DummySeeder extends Seeder
      */
     public function run()
     {
-        foreach(App\Site::all() as $site){
+        $firstUser = App\User::first();
+        $rawSites = [
+            [
+                'name' => 'Wikipedia.com',
+                'user_id' => $firstUser->id,
+                'currency' => rand(0, 3)
+            ],
+            [
+                'name' => 'ProductHunt.com',
+                'user_id' => $firstUser->id,
+                'currency' => rand(0, 3)
+            ],
+            [
+                'name' => 'news.YCombinator.com',
+                'user_id' => $firstUser->id,
+                'currency' => rand(0, 3)
+            ],
+        ];
+
+        $sites = [];
+        foreach($rawSites as $site){
+            $sites[] = App\Site::create($site);
+        }
+
+        foreach($sites as $site){
             $noOfUsers = rand(10, 30);
             factory(App\Author::class, $noOfUsers)->create()->each(function ($u) use ($site){
                 $u->generateToken();
